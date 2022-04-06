255413bf832fb2b24fe0518087cb8125238e223b
DimaXDD <dimatruba2004@yandex.ru>
2022-04-07 00:33:13 +0300

    Add Lab7

---
+++ Lab7_Вычисление сумм, произведений, экстремумов/Lab7_15_dop6/Lab7_15_dop6.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+int main()
+{
+	using namespace std;
+	setlocale(LC_ALL, "RUS");
+	int n, x, summa = 0, proizv = 1;
+	cout << "Из скольки элементов дана последовательность? ";
+	cin >> n;
+	if (n > 0)
+	{
+		for (int a = 1; a <= n; a++)
+		{
+			cout << "Введите x = ";
+			cin >> x;
+			if (x >= 0)
+			{
+				summa += x;
+			}
+			else
+			{
+				proizv *= x;
+			}
+		}
+	}
+	else
+	{
+		cout << "Неккоректные данные" << endl;
+	}
+	cout << "Сумма положительных элементов = " << summa << endl;
+	cout << "Произведение отрицательных элементов = " << proizv;
+	return (0);
+}
---
+++ Lab7_Вычисление сумм, произведений, экстремумов/Lab7_15_dop8/Lab7_15_dop8.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+int main()
+{
+	using namespace std;
+	setlocale(LC_ALL, "RUS");
+	int n, x, summa = 0, sred, chetchik = 0;
+	cout << "Из скольки элементов дана последовательность? ";
+	cin >> n;
+	if (n > 0)
+	{
+		for (int a = 1; a <= n; a++)
+		{
+			cout << "Введите x = ";
+			cin >> x;
+			if (a % 2 == 1)
+			{
+				summa += x;
+				chetchik++;
+			}
+		}
+	}
+	else
+	{
+		cout << "Неккоректные данные" << endl;
+	}
+	sred = summa / chetchik;
+	cout << "Количество нечётных элементов = " << chetchik << endl;
+	cout << "Их среднее арифметическое = " << sred;
+	return (0);
+}
