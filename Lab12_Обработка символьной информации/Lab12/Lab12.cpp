334ac8260de3aae0067c756121c4d4723875e794
DimaXDD <dimatruba2004@yandex.ru>
2022-04-07 00:36:21 +0300

    Add Lab12

---
+++ Lab12_Обработка символьной информации/Lab12/Lab12.cpp
@@ -0,0 +1,19 @@
+﻿//Из предложения удалить все символы, совпадающие с символом, введенным с клавиатуры.
+#include <iostream>
+#include <string>
+int main() {
+    using namespace std;
+    setlocale(LC_ALL, "RUS");
+    string s;
+    cout << "Введите заданное предложение: ";
+    cin >> s;
+    char c;
+    cout << "Введите символ, который нужно удалить: ";
+    cin >> c;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == c) {
+            s.erase(i, 1); i--;
+        }
+    }
+    cout << s;
+}
---
+++ Lab12_Обработка символьной информации/Lab12_main1/Lab12_main1.cpp
@@ -0,0 +1,28 @@
+﻿//Написать программу, реализующую выделение подстроки S1 длиной k с позиции номер n из строки. 
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	string s;
+	string s1;
+	int n, k;
+	cout << "Введите заданное предложение: ";
+	cin >> s;
+	cout << "Введите кол-во символов в подстроке s1: ";
+	cin >> k;
+	cout << "Введите, с которого символа нужно вырезать подстроку: ";
+	cin >> n;
+	for (int i = 0; i < s.length(); i++)
+	{
+		for (int i1 = 0; i1 < k; i1++)
+		{
+			s.substr(0, k);
+		}
+		s.erase(n-1, k); i -= k;
+	}
+	cout << "Подстрока: " << s1 << endl;
+	cout << "Что осталось: " << s << endl;
+}
